639ed1ae5ad75b10c35c84332f8a0fdf48c3d07b
hariharandhakshnamurthi <113970274+hariharandhakshnamurthi@users.noreply.github.com>
2022-09-28 02:43:39 -0400

    Add files via upload

---
+++ assignment3.py
@@ -0,0 +1,18 @@
+Temperature=int(input('Temperature = '))
+Humidity=int(input('Humidity = '))
+if(Temperature>=50):
+    {
+        print('Temperature is high')
+    }
+else:
+    {
+        print('Temperature is normal')
+    }
+if(Humidity>=500):
+    {
+        print('Humidity is high')
+    }
+else:
+    {
+        print('Humidity is normal')
+    }
